c0c3f18207ea8cfb66db299d0e4e159fa9d32f55
Ludvig Björk Förare <luddebf@gmail.com>
2019-11-24 00:12:27 +0100

    Added main menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void BTNStart()
+    {
+        SceneManager.LoadScene("InGame");
+    }
+
+    public void BTNExit()
+    {
+        Application.Quit();
+    }
+    public void BTNFullScreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+    public void BTNTeamTrees()
+    {
+        Application.OpenURL("https://teamtrees.org/");
+    }
+}
